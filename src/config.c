539209c2a96edf2379c1b75f93da7516a9453abb
Seas0 <30485844+Seas0@users.noreply.github.com>
2022-01-21 15:25:43 +0800

    WIP: Prototype, wait for rebase

--- src/config.c
+++ src/config.c
@@ -7,10 +7,75 @@
 #include <errno.h>
 #include <unistd.h>
 
+#include "box86context.h"
+#include "fileutils.h"
+#include "pathcoll.h"
 #include "config.h"
 
-const char *config_dir = "/etc/box86/";
+typedef struct config_s config_t;
+config_t *config_table;
+size_t config_table_size;
 
+const char *local_config_path = "box86.conf";
 
+const char *system_config_path = "/etc/box86/box86.conf";
 
-// only a dummy file now, wait for implementation
+inline static int8_t isSpace(char c)
+{
+    return c == ' ' || c == '\r' || c == '\x09';
+}
+inline static int8_t isValidCharacter(char c)
+{
+    return ('0' <= c && c <= '9') ||
+           ('A' <= c && c <= 'Z') ||
+           ('a' <= c && c <= 'z') ||
+           c == '_' || c == '.';
+}
+inline static int8_t boolEval(char *c)
+{
+    if (!strcasecmp(c, "true") || !strcasecmp(c, "yes") || !strcasecmp(c, "on") || !strcasecmp(c, "1"))
+        return 1;
+    if (!strcasecmp(c, "false") || !strcasecmp(c, "no") || !strcasecmp(c, "off") || !strcasecmp(c, "0"))
+        return 0;
+    return -1;
+}
+
+/*
+    Parse a ini file
+*/
+int ParseConfigFile(FILE *file, config_t **config, size_t *n)
+{
+}
+
+/*
+    Find and read config
+*/
+int InitConfigFile()
+{
+    char *user_config_path = getenv("HOME");
+    strcat(user_config_path, "/.box86/box86.conf");
+
+    const char *config_paths[] = {local_config_path,
+                                  user_config_path,
+                                  system_config_path};
+
+    for (int i = 0; i < 3; i++)
+    {
+        config_t *config;
+        size_t size;
+        FILE *file = fopen(config_paths[i], "r");
+        if (!file)
+            continue;
+        if (ParseConfigFile(file, &config, &size))
+        {
+            fclose(file);
+            continue;
+        }
+        fclose(file);
+        config_table = realloc(config_table, config_table_size + size * sizeof(config_t));
+        memcpy(config_table + config_table_size * sizeof(config_t), config, size * sizeof(config_t));
+        config_table_size += size;
+    }
+
+    return 0;
+}
--- src/include/config.h
+++ src/include/config.h
@@ -1,81 +1,150 @@
 #ifndef __CONFIG_H__
 #define __CONFIG_H__
 
+#include <stdio.h>
 #include <stdint.h>
 
+typedef enum
+{
+    Undefined = -1,
+    False = 0,
+    True = 1
+} tristate_t;
 typedef struct config_s
 {
     char *program_name;
 
-    int no_banner;
-    int log_level;
-    int dump;
+    tristate_t no_banner;
+    tristate_t log_level;
+    tristate_t dump;
 #ifdef DYNAREC
-    int dynarec_log_level;
-    int dynarec_dump;
-    int dynarec_enabled;
-    int dynarec_forced_all_addr;
+    tristate_t dynarec_log_level;
+    tristate_t dynarec_dump;
+    tristate_t dynarec_enabled;
+    tristate_t dynarec_forced_all_addr;
     uintptr_t no_dynarec_start;
     uintptr_t no_dynarec_end;
 
-    int dynarec_bigblock;
-    int dynarec_strongmem;
+    tristate_t dynarec_bigblock;
+    tristate_t dynarec_strongmem;
 #endif
 
 #ifdef HAVE_TRACE
-    int trace_xmm;
-    int trace_emm;
-    int trace_start;
+    tristate_t trace_xmm;
+    tristate_t trace_emm;
+    unsigned long long trace_start;
 #ifdef DYNAREC
-    int trace_dynarec;
+    tristate_t trace_dynarec;
 #endif
 #endif
-    int dlsym_error;
+    tristate_t dlsym_error;
 
-    int x11_color16;
-    int x11_threads;
-    int x11_glx;
+    tristate_t x11_color16;
+    tristate_t x11_threads;
+    tristate_t x11_glx;
 
     char *libgl;
 
-    int allow_missing_libs;
+    tristate_t allow_missing_libs;
 
-    int no_pulse;
-    int no_gtk;
-    int no_vulkan;
+    tristate_t no_pulse;
+    tristate_t no_gtk;
+    tristate_t no_vulkan;
 
-    int fix_64bit_inodes;
+    tristate_t fix_64bit_inodes;
 
-    int jit_gdb;
+    tristate_t jit_gdb;
 
-    char *lib_path;
-    int lib_path_merge;
-    char *path;
-    int path_merge;
+    path_collection_t *path;
+    tristate_t path_merge;
+    path_collection_t *lib_path;
+    tristate_t lib_path_merge;
+    path_collection_t *lib_preload;
+    tristate_t lib_preload_merge;
 
-    char *emulated_libs;
+    path_collection_t *emulated_libs;
 
-    int no_sigsegv;
-    int no_sigill;
+    tristate_t no_sigsegv;
+    tristate_t no_sigill;
 
-    int trace;
-    int trace_init;
+    tristate_t trace;
+    tristate_t trace_init;
     char *trace_file_path;
 
 } config_t;
+const config_t config_default = {
+    .program_name = NULL,
+    .no_banner = Undefined,
+    .log_level = Undefined,
+    .dump = Undefined,
+#ifdef DYNAREC
+    .dynarec_log_level = Undefined,
+    .dynarec_dump = Undefined,
+    .dynarec_enabled = Undefined,
+    .dynarec_forced_all_addr = Undefined,
+    .no_dynarec_start = Undefined,
+    .no_dynarec_end = Undefined,
 
-typedef struct library_config_s
-{
-    char *library_name;
-    char *value;
-} library_config_t;
+    .dynarec_bigblock = Undefined,
+    .dynarec_strongmem = Undefined,
+#endif
 
-// parse a config file
-config_t *config_parse(FILE *file);
+#ifdef HAVE_TRACE
+    .trace_xmm = Undefined,
+    .trace_emm = Undefined,
+    .trace_start = Undefined,
+#ifdef DYNAREC
+    .trace_dynarec = Undefined,
+#endif
+#endif
+    .dlsym_error = Undefined,
+    .x11_color16 = Undefined,
+    .x11_threads = Undefined,
+    .x11_glx = Undefined,
 
-// parse a section of a config file
-int config_parse_section(config_t *config, FILE *file);
+    .libgl = NULL,
 
-// only a dummy file now, wait for implementation
+    .allow_missing_libs = Undefined,
+    .no_pulse = Undefined,
+    .no_gtk = Undefined,
+    .no_vulkan = Undefined,
+    .fix_64bit_inodes = Undefined,
+    .jit_gdb = Undefined,
+
+    .path = NULL,
+    .path_merge = Undefined,
+    .lib_path = NULL,
+    .lib_path_merge = Undefined,
+    .lib_preload = NULL,
+    .lib_preload_merge = Undefined,
+
+    .emulated_libs = NULL,
+
+    .no_sigsegv = Undefined,
+    .no_sigill = Undefined,
+
+    .trace = Undefined,
+    .trace_init = Undefined,
+    .trace_file_path = NULL};
+
+extern config_t *config_table;
+
+// Parse a config file
+int ParseConfigFile(FILE *file, config_t **config, size_t *n);
+
+// Parse a section of a config file
+int ParseConfigSection(FILE *file, config_t *config);
+
+// Parse a line of a config file
+int ParseConfigLine(char *line, config_t *config);
+
+// Initialize config
+int InitConfigFile();
+
+// Free config
+void FreeConfig();
+
+// Get Option
+char *GetOption(const char *name);
 
 #endif
